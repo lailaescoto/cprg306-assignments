081af6e2b755da6f60ae7fab68d0b03b3e959ba2
Laila <laila.escoto1@gmail.com>
2025-01-20 22:17:06 -0700

    fixed root page, added page.js and the import for studentinfo, added studentinfo.js + my name and the github repo link

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link"; 
+
 export default function Page() {
     return (
       <div>
-        <h1>Hello world</h1>
-        <p>This is a simple web page.</p>
+        <h1>CPRG 306: Web Development 2 - Assignments</h1>        
+        <p>
+          Link to Week 2: <Link href="week-2">Week 2 </Link>
+        </p>
       </div>
     );
   }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo () {
+    return ( 
+    <div>
+    <h1>Laila Angelica Escoto</h1>
+    <p><Link href="https://github.com/lailaescoto?tab=repositories">https://github.com/lailaescoto?tab=repositories</Link></p>
+    </div>
+    );
+}
