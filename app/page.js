eddecaf5a0e78e3ab3a8d3cea52d08045e0815b3
Laila <laila.escoto1@gmail.com>
2025-02-20 02:12:02 -0700

    set up item list + finished week 6 reqs

--- app/page.js
+++ app/page.js
@@ -16,6 +16,9 @@ export default function Page() {
         <p>
           Link to Week 5: <Link href="week-5">Week 5 </Link>
         </p>
+        <p>
+          Link to Week 6: <Link href="week-6">Week 6 </Link>
+        </p>
       </div>
     );
   }
